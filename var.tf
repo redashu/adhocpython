c4c8a8e1cf3bde5770eaa4d37d0c2898f6fc80b1
Ashutoshh Singh <asashuthegreat4@gmail.com>
2025-07-24 15:48:56 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,16 @@
+terraform { 
+  cloud { 
+    
+    organization = "delvex" 
+
+  } 
+}
+
+
+resource "null_resource" "example" {
+    
+    provisioner "local-exec" {
+        command = "echo 'This is a null resource example.'"
+    }
+  
+}
