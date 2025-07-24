1183390109fed788312cfc9b198e13a7b2d3fec7
Ashutoshh Singh <asashuthegreat4@gmail.com>
2025-07-24 23:58:49 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -10,7 +10,7 @@ terraform {
 resource "null_resource" "example" {
     
     provisioner "local-exec" {
-        command = "echo 'This is a null resource example. with terraform cloud'"
+        command = "echo 'This is a null resource example. with terraform cloud with github'"
     }
   
 }
